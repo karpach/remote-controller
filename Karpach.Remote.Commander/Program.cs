b1c16363160a9b29917029b683f8f98db44ab205
Viktar Karpach <vkarpach@gmail.com>
2017-11-06 22:24:54 -0600

    Add fatal error logging

--- Karpach.Remote.Commander/Program.cs
+++ Karpach.Remote.Commander/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Windows.Forms;
 using Autofac;
-using Autofac.Builder;
 using Karpach.Remote.Commander.Helpers;
 using Karpach.Remote.Commander.Interfaces;
+using NLog;
 
 namespace Karpach.Remote.Commander
 {
     static class Program
     {
         public static IContainer Container { get; set; }
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         /// <summary>
         /// The main entry point for the application.
@@ -17,17 +18,25 @@ namespace Karpach.Remote.Commander
         [STAThread]
         static void Main()
         {
-            var builder = new ContainerBuilder();            
-            builder.RegisterType<ControllerApplicationContext>().AsSelf();
-            builder.RegisterType<SettingsForm>().AsSelf();
-            builder.RegisterType<HostHelper>();
-            builder.RegisterType<CommandsSettings>().As<ICommandsSettings>();
-            builder.RegisterType<CommandsManager>();                   
-            builder.RegisterType<SettingsForm>();
-            Container = builder.Build();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(Container.Resolve<ControllerApplicationContext>());
+            try
+            {
+                var builder = new ContainerBuilder();
+                builder.RegisterType<ControllerApplicationContext>().AsSelf();
+                builder.RegisterType<SettingsForm>().AsSelf();
+                builder.RegisterType<HostHelper>();
+                builder.RegisterType<CommandsSettings>().As<ICommandsSettings>();
+                builder.RegisterType<CommandsManager>();
+                builder.RegisterType<SettingsForm>();
+                Container = builder.Build();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(Container.Resolve<ControllerApplicationContext>());
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(LogLevel.Fatal, ex);
+                MessageBox.Show("Encountered a fatal error, please see log file for details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }            
         }
     }
 }
